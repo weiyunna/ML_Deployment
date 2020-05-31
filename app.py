8b252101a10cdf67380731843e02aec9d226aeec
weiyunna <yunna_wei@jabil.com>
2020-05-31 10:25:16 +0800

    revise the api method

--- app.py
+++ app.py
@@ -31,10 +31,11 @@ def predict():
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)
-    final = np.array([int_features])    
-    prediction = model.predict(final)
+    data_unseen = pd.DataFrame([data])
+    #final = np.array([int_features])    
+    prediction = model.predict(data_unseen)
     prediction = round(prediction[0], 2)
-    #data_unseen = pd.DataFrame([data])
+    
     #prediction = predict_model(model, data=data_unseen)
     #output = prediction.Label[0]
     return jsonify(prediction)
